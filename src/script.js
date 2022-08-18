81397a0611211a3a28adfa9976adce1d3bb058e7
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2022-08-18 21:17:40 +0530

    add

--- src/script.js
+++ src/script.js
@@ -1,4 +1,9 @@
+import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as dat from 'dat.gui'
+
+const gui = new dat.GUI()
 
 let cursor = {
     x: 0,
@@ -18,20 +23,53 @@ const material = new THREE.MeshBasicMaterial({ color: 'red' })
 
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
+const parameters = {
+    color: 0xff0000
+}
+gui.add(cube.position, 'x', -10, 10, 0.1)
+gui.add(cube.position, 'y', -10, 10, 0.1)
+gui.add(cube.position, 'z', -10, 10, 0.1)
+gui
+    .addColor(parameters, 'color')
+    .onChange(() => {
+        material.color.set(parameters.color)
+    })
 
 const sizes = {
-    width: 800,
-    height: 700
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+
+window.addEventListener('resize', event => {
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+})
+
+window.addEventListener('dblclick', event => {
+    if (document.fullscreenElement) {
+        document.exitFullscreen()
+    } else {
+        canvas.requestFullscreen()
+    }
+})
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
 camera.position.z = 5
 scene.add(camera)
 
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(window.devicePixelRatio)
 
 let time = Date.now()
 
@@ -40,9 +78,7 @@ function animate() {
     const deltaTime = currentTime - time
     time = currentTime
 
-    camera.position.x = cursor.x * 10
-    camera.position.y = -cursor.y * 10
-    camera.lookAt(cube.position)
+    controls.update()
 
     renderer.render(scene, camera)
     requestAnimationFrame(animate)
