b3a143464aae1de675b9360562a5eaf4060452f0
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2022-08-19 19:45:48 +0530

    shadow

--- src/script.js
+++ src/script.js
@@ -18,53 +18,48 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-const parameters = {
-    color: 0xffffff
-}
+// Ambient light
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
+
+// Directional light
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+directionalLight.castShadow = true
+directionalLight.position.set(2, 2, - 1)
+gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
+gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
+gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
+gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(directionalLight)
 
-gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
-gui.addColor(parameters, 'color')
-    .onChange(() => {
-        // ambientLight.color.set(parameters.color)
-        directionalLight.color.set(parameters.color)
-    })
+/**
+ * Materials
+ */
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.7
+gui.add(material, 'metalness').min(0).max(1).step(0.001)
+gui.add(material, 'roughness').min(0).max(1).step(0.001)
+
 /**
  * Objects
  */
-// Material
-const material = new THREE.MeshStandardMaterial()
-material.roughness = 0.4
-
-// Objects
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 32, 32),
     material
 )
-sphere.position.x = - 1.5
-
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(0.75, 0.75, 0.75),
-    material
-)
-
-const torus = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
-    material
-)
-torus.position.x = 1.5
+sphere.castShadow = true
 
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
     material
 )
-plane.rotation.x = - Math.PI * 0.5
-plane.position.y = - 0.65
+plane.receiveShadow = true
 
-scene.add(sphere, cube, torus, plane)
+plane.rotation.x = - Math.PI * 0.5
+plane.position.y = - 0.5
+
+scene.add(sphere, plane)
 
 /**
  * Sizes
@@ -110,6 +105,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true
 
 /**
  * Animate
@@ -118,15 +114,6 @@ const clock = new THREE.Clock()
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    sphere.rotation.y = 0.1 * elapsedTime
-    cube.rotation.y = 0.1 * elapsedTime
-    torus.rotation.y = 0.1 * elapsedTime
-
-    sphere.rotation.x = 0.15 * elapsedTime
-    cube.rotation.x = 0.15 * elapsedTime
-    torus.rotation.x = 0.15 * elapsedTime
 
     // Update controls
     controls.update()
